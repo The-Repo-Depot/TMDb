f2944b5efb2e596d5202da83b2e56a358d16f989
Tushar <tsaha.cse@gmail.com>
2017-06-13 22:13:18 +0600

    - TvShowRecyclerView scrolling behavior added - Activity-Presenter interfacing methods added

--- app/src/main/java/tushar/letgo/tmdb/feature/tvshowlisting/TvShowListingActivity.java
+++ app/src/main/java/tushar/letgo/tmdb/feature/tvshowlisting/TvShowListingActivity.java
@@ -2,27 +2,44 @@ package tushar.letgo.tmdb.feature.tvshowlisting;
 
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ProgressBar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.Bind;
 import tushar.letgo.tmdb.R;
 import tushar.letgo.tmdb.common.mvp.PresenterActivity;
+import tushar.letgo.tmdb.common.view.EndlessRecyclerOnScrollListener;
+import tushar.letgo.tmdb.model.TvShow;
 
-public class TvShowListingActivity extends PresenterActivity implements TvShowListingView, SwipeRefreshLayout.OnRefreshListener {
+public class TvShowListingActivity extends PresenterActivity implements TvShowListingView, SwipeRefreshLayout.OnRefreshListener, OnTvShowClickListener {
 
     @Bind(R.id.swipe_refresh_layout)
-    SwipeRefreshLayout swipeRefreshLayout;
+    SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Bind(R.id.tv_list)
-    RecyclerView tvList;
+    RecyclerView mTvShowRecyclerView;
 
     @Bind(R.id.progress_bar)
-    ProgressBar progressBar;
+    ProgressBar mProgressBar;
 
     @Bind(R.id.view_error_no_content)
-    View errorNoContentView;
+    View mErrorNoContentView;
+
+    private GridLayoutManager mGridLayoutManager;
+
+    private TvShowRecyclerViewAdapter mTvShowRecyclerViewAdapter;
+
+    private EndlessRecyclerOnScrollListener mEndlessRecyclerOnScrollListener;
+
+    private EndlessRecyclerOnScrollListener.State state = new EndlessRecyclerOnScrollListener.State();
+
+
+    List<TvShow> tvShows = new ArrayList<>();
 
     @Override
     protected int getLayoutResourceId() {
@@ -33,10 +50,112 @@ public class TvShowListingActivity extends PresenterActivity implements TvShowLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        initSwipeRefreshLayout();
+        initRecyclerView();
+    }
+
+    private void initSwipeRefreshLayout() {
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.colorAccent);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+    }
+
+    private void initRecyclerView() {
+        mTvShowRecyclerViewAdapter = new TvShowRecyclerViewAdapter(this, tvShows, this);
+
+        mTvShowRecyclerView.setAdapter(mTvShowRecyclerViewAdapter);
+        mGridLayoutManager = new GridLayoutManager(this, getResources().getInteger(R.integer.tv_show_columns));
+        mGridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                int itemViewType = mTvShowRecyclerViewAdapter.getItemViewType(position);
+
+                switch (itemViewType) {
+                    case TvShowRecyclerViewAdapter.VIEW_TYPE_ITEM:
+                        return 1;
+                    case TvShowRecyclerViewAdapter.VIEW_TYPE_PROGRESS:
+                        return getResources().getInteger(R.integer.tv_show_columns);
+                    default:
+                        return -1;
+                }
+            }
+        });
+        mTvShowRecyclerView.setLayoutManager(mGridLayoutManager);
+
+        mEndlessRecyclerOnScrollListener = new EndlessRecyclerOnScrollListener(mTvShowRecyclerView) {
+            @Override
+            public boolean onLoadMore(int page, int totalItemsCount) {
+                return true;
+            }
+
+            @Override
+            public void enableRefresh() {
+                mSwipeRefreshLayout.setEnabled(true);
+            }
+
+            @Override
+            public void disableRefresh() {
+                mSwipeRefreshLayout.setEnabled(false);
+            }
+
+            @Override
+            public void onScroll() {
+                // skip
+            }
+        };
+
+        mEndlessRecyclerOnScrollListener.setState(state);
+        mTvShowRecyclerView.addOnScrollListener(mEndlessRecyclerOnScrollListener);
     }
 
     @Override
     public void onRefresh() {
 
+    }
+
+    @Override
+    public void onTvShowSelected(View selectedView, TvShow tvShow) {
+
+    }
+
+    @Override
+    public void showProgress() {
+        mSwipeRefreshLayout.setVisibility(View.GONE);
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        mSwipeRefreshLayout.setVisibility(View.VISIBLE);
+        mProgressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void showAdsLoadingProgress() {
+        mTvShowRecyclerViewAdapter.add(ProgressTvShow.INSTANCE);
+    }
+
+    @Override
+    public void hideAdsLoadingProgress() {
+        mTvShowRecyclerViewAdapter.remove(ProgressTvShow.INSTANCE);
+    }
+
+    @Override
+    public void showAds(List<TvShow> tvShows) {
+        mTvShowRecyclerViewAdapter.addAll(tvShows);
+    }
+
+    @Override
+    public void hideErrors() {
+        mErrorNoContentView.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void showTvShowLoadingError(String reason) {
+        // skip
+    }
+
+    @Override
+    public void showTvShowLoadingError() {
+        mErrorNoContentView.setVisibility(View.VISIBLE);
     }
 }
--- app/src/main/java/tushar/letgo/tmdb/feature/tvshowlisting/TvShowListingView.java
+++ app/src/main/java/tushar/letgo/tmdb/feature/tvshowlisting/TvShowListingView.java
@@ -1,8 +1,28 @@
 package tushar.letgo.tmdb.feature.tvshowlisting;
 
+import java.util.List;
+
+import tushar.letgo.tmdb.model.TvShow;
+
 /**
  * Created by Tushar on 6/13/17.
  */
 
 public interface TvShowListingView {
+
+    void showProgress();
+
+    void hideProgress();
+
+    void showAdsLoadingProgress();
+
+    void hideAdsLoadingProgress();
+
+    void showAds(List<TvShow> tvShows);
+
+    void hideErrors();
+
+    void showTvShowLoadingError(String reason);
+
+    void showTvShowLoadingError();
 }
